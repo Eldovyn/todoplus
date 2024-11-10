4aacf2fb7756e7dd39770cedd27adf8a8d6f0afb
adit <farras.pramudita@gmail.com>
2024-11-10 19:39:36 +0700

    feat(page): add icon at dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 "use client";
-
 import React from 'react';
+import IconWeb from '../../../public/IconRemoverBg.png'
+import Image from 'next/image'
+import { FaTasks } from "react-icons/fa";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { LuHistory } from "react-icons/lu";
 
 const Sidebar = () => {
   // Event handler untuk meng-handle klik pada setiap item
@@ -11,8 +15,9 @@ const Sidebar = () => {
   return (
     <div className="flex min-h-screen bg-gray-100">
       <aside className="w-64 bg-gray-900 text-white flex flex-col">
-        <div className="p-4 flex items-center justify-center">
-          <span className="text-xl font-semibold">Menu</span>
+        <div className='flex justify-start p-5'>
+          <Image src={IconWeb} alt='' className='w-11 me-2' />
+          <p className='font-bold text-lg'>TodoPlus</p>
         </div>
         <nav className="flex-1 px-2">
           <ul className="space-y-2">
@@ -23,7 +28,8 @@ const Sidebar = () => {
                 className="flex items-center px-2 py-2 text-gray-200 hover:bg-gray-700 rounded"
               >
                 <i className="bi bi-house text-lg mr-2"></i>
-                <span className="hidden sm:inline">Home</span>
+                <FaTasks />
+                <span className="hidden sm:inline ms-2">Task</span>
               </a>
             </li>
             <li>
@@ -33,27 +39,19 @@ const Sidebar = () => {
                 className="flex items-center px-2 py-2 text-gray-200 hover:bg-gray-700 rounded"
               >
                 <i className="bi bi-speedometer2 text-lg mr-2"></i>
-                <span className="hidden sm:inline">Dashboard</span>
+                <LuHistory />
+                <span className="hidden sm:inline ms-2">History</span>
               </a>
             </li>
             <li>
               <a
                 href="#"
-                onClick={() => handleItemClick("Orders")}
+                onClick={() => handleItemClick("Dashboard")}
                 className="flex items-center px-2 py-2 text-gray-200 hover:bg-gray-700 rounded"
               >
-                <i className="bi bi-table text-lg mr-2"></i>
-                <span className="hidden sm:inline">Orders</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                onClick={() => handleItemClick("Customers")}
-                className="flex items-center px-2 py-2 text-gray-200 hover:bg-gray-700 rounded"
-              >
-                <i className="bi bi-people text-lg mr-2"></i>
-                <span className="hidden sm:inline">Customers</span>
+                <i className="bi bi-speedometer2 text-lg mr-2"></i>
+                <MdOutlineAccountCircle />
+                <span className="hidden sm:inline ms-2">Account</span>
               </a>
             </li>
           </ul>
@@ -67,12 +65,6 @@ const Sidebar = () => {
             />
             <span className="hidden sm:inline">loser</span>
           </div>
-          <ul className="mt-2 text-sm text-gray-400">
-            <li><a href="#" className="block hover:text-white">New project...</a></li>
-            <li><a href="#" className="block hover:text-white">Settings</a></li>
-            <li><a href="#" className="block hover:text-white">Profile</a></li>
-            <li><a href="#" className="block hover:text-white">Sign out</a></li>
-          </ul>
         </div>
       </aside>
       <main className="flex-1 p-6">
