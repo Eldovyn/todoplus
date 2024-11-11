bf38f08efe5c7ac7c0fae877ca49e0f7ed0c4709
Eldovyn <farras.pramudita@gmail.com>
2024-11-12 00:56:03 +0700

    feat(Account Page): update navbar account page

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const AccountPage: React.FunctionComponent = () => {
     return (
         <>
-            {/* <p className="text-black">hello world</p> */}
-            {/* <hr className="border-gray-300 w-full mt-[4.3rem]" /> */}
-            <div className="flex flex-col mb-3 space-y-2 w-full">
-                <div className="p-2">Flex item 1</div>
-                <hr className="border-gray-300 w-full" />
+            <div className="max-w-screen-xl mx-auto w-full bg-white">
+                <div className="flex flex-col space-y-2 w-full bg-white mt-5 h-screen">
+                    <div className="p-2 text-right">Flex item 1</div>
+                    <hr className="border-gray-300" />
+                </div>
             </div>
         </>
     )
