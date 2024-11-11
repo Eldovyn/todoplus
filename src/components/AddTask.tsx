c87e8cf3211bb753172449312144bc87275b3a7a
adit <farras.pramudita@gmail.com>
2024-11-11 08:34:11 +0700

    fix(add task): remove field label and update placeholder from Email Address to Task

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -8,28 +8,19 @@ const AddTask: React.FunctionComponent = () => {
                     <form action="">
                         <div className="mb-3 mt-3">
                             <input
-                                type="email"
+                                type="text"
                                 id="exampleInputEmail1"
                                 className="mt-1 block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-full mx-auto"
-                                placeholder="Email Address"
+                                placeholder="Task"
                                 aria-describedby="emailHelp"
                             />
                         </div>
                         <div className="mb-3 mt-3">
                             <input
-                                type="email"
+                                type="text"
                                 id="exampleInputEmail1"
                                 className="mt-1 block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-full mx-auto"
                                 placeholder="Description"
-                                aria-describedby="emailHelp"
-                            />
-                        </div>
-                        <div className="mb-3 mt-3">
-                            <input
-                                type="email"
-                                id="exampleInputEmail1"
-                                className="mt-1 block px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-full mx-auto"
-                                placeholder="Label"
                                 aria-describedby="emailHelp"
                             />
                         </div>
