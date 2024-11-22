9e8be62679f9375de3236ddbe06aa53b9ea2fcf9
Eldovyn <farras.pramudita@gmail.com>
2024-11-22 23:00:31 +0700

    feat(page): add page profile

---
+++ src/components/FormProfile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const FormProfile: React.FC = () => {
+    return (
+        <>
+            <form className="flex flex-col text-gray-900 w-[47%]">
+                <p className="text-left text-sm pt-3 text-gray-300 pb-1">Email</p>
+                <input
+                    type="text"
+                    className="block w-full px-4 py-2 text-base text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    id="profileInput"
+                    placeholder="name@example.com"
+                />
+                <p className="text-left text-sm pt-5 text-gray-300 pb-1">Username</p>
+                <input
+                    type="text"
+                    className="block w-full px-4 py-2 text-base text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    id="passwordInput"
+                    placeholder="name@example.com"
+                />
+                <button type="submit" className="mt-2 px-4 py-2 bg-blue-600 text-white rounded-md">Submit Profile</button>
+            </form>
+        </>
+    )
+};
+
+export default FormProfile;
