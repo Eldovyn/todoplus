245b054b2e6210eda5b2941395c543e2e830471a
Eldovyn <farras.pramudita@gmail.com>
2024-11-23 01:01:36 +0700

    feat(api): add api change email & username

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,30 +2,19 @@ interface UserOptions {
     [key: string]: any;
 }
 
-const apiUpdateUser = async (category: string, accessToken: string, options: UserOptions) => {
-    let data;
-    if (category === 'email') {
-        data = {
-            new_email: options.email
-        }
-    } else if (category === 'username') {
-        data = {
-            new_username: options.username
-        }
-    }
-    let response = await fetch(`${process.env.NEXT_PUBLIC_TODOPLUS_API}todoplus/profile/${category}`, {
+const apiUpdateUserProfile = async (accessToken: string, options: UserOptions) => {
+    let response = await fetch(`${process.env.NEXT_PUBLIC_TODOPLUS_API}todoplus/profile`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${accessToken}`
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(options)
     })
     return response;
 }
 
 const apiGetUser = async (accessToken: string) => {
-    console.log(accessToken, process.env.NEXT_PUBLIC_TODOPLUS_API)
     let response = await fetch(`${process.env.NEXT_PUBLIC_TODOPLUS_API}todoplus/@me`, {
         method: 'GET',
         headers: {
@@ -36,4 +25,4 @@ const apiGetUser = async (accessToken: string) => {
     return response;
 }
 
-export { apiUpdateUser, apiGetUser }
+export { apiUpdateUserProfile, apiGetUser }
