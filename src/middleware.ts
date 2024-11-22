ca7e06f82af2c37dc17cb0bb79535083c967ec0f
Eldovyn <farras.pramudita@gmail.com>
2024-11-22 13:27:59 +0700

    fix(middleware): update middleware if token is null & token invalid

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,22 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { jwtVerify } from 'jose';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
     const accessToken = request.cookies.get('accessToken');
+    const secret = process.env.SECRET_KEY;
 
     if (pathname === '/') {
         if (!accessToken) {
             return NextResponse.redirect(new URL('/login', request.url));
+        }
+        try {
+            const { payload } = await jwtVerify(accessToken.value, new TextEncoder().encode(secret));
+        } catch (error) {
+            const response = NextResponse.redirect(new URL('/login', request.url));
+            response.cookies.delete('accessToken');
+            return response;
         }
     }
 
