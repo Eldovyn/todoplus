abbb223a71c0e73f1d8ba908c5c2241fb19b310f
Eldovyn <farras.pramudita@gmail.com>
2024-12-20 20:01:40 +0700

    feat(home, history): update container

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -96,60 +96,62 @@ function HistoryPage() {
     return (
         <>
             <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
-            <div className='bg-gray-900 mt-[10rem] mb-3 text-white font-semibold rounded-md flex justify-center items-center w-[45%] mx-auto p-4'>
-                <MdOutlineHistoryToggleOff size={25} />
-                <div className="me-1 ms-1">History</div>
-            </div>
-            <div className="flex items-center justify-end me-[27.6%]">
-                <div className="relative w-[13%]">
-                    <input
-                        type="text"
-                        placeholder="Cari Title...."
-                        className="border-gray-900 border rounded-md mt-3 mb-3 w-full p-2 pr-10 active:border-gray-900"
-                        onKeyDown={loadingSearch ? () => {} : handleKeyDown}
-                        onChange={(e) => setSearchTitle(e.target.value)}
-                    />
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-4.35-4.35m2.35-8a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
+            <div className="container mx-auto px-4 mt-[5rem]">
+                <div className='bg-gray-900 mb-3 text-white font-semibold rounded-md flex justify-center items-center w-[45%] mx-auto p-4'>
+                    <MdOutlineHistoryToggleOff size={25} />
+                    <div className="me-1 ms-1">History</div>
+                </div>
+                <div className="flex items-center justify-end me-[27.6%]">
+                    <div className="relative w-[13%]">
+                        <input
+                            type="text"
+                            placeholder="Cari Title...."
+                            className="border-gray-900 border rounded-md mt-3 mb-3 w-full p-2 pr-10 active:border-gray-900"
+                            onKeyDown={loadingSearch ? () => { } : handleKeyDown}
+                            onChange={(e) => setSearchTitle(e.target.value)}
+                        />
+                        <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-4.35-4.35m2.35-8a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <br />
-            <hr className='mx-auto w-[45%]' />
-            <br />
-            {loading ? (
-                <LoadingSpinnerComponent type={'Spinner'} color={'black'} size={'50px'} />
-            ) : (
-                listTask[currentPage] && listTask[currentPage].length > 0 ? (
-                    listTask[currentPage].map((item: any) => (
-                        <div key={item.task_id} className="border rounded-lg shadow p-4 text-white w-[45%] mx-auto m-5 bg-gray-900 flex justify-between items-center">
-                            <p className={`text-sm ${item.is_completed ? 'line-through' : ''}`}>{item.title}</p>
-                            <div className="flex flex-row items-center">
-                                <TrashTask item={item} setListTask={setListTask} setTotalPage={setTotalPage} />
-                                <MdEdit size={18} className="m-1 cursor-pointer" onClick={() => openModal(item.task_id)} />
-                                <Completed item={item} setListTask={setListTask} setTotalPage={setTotalPage} setItemsPerPage={setItemsPerPage} setTotalItems={setTotalItems} setLimit={setLimit} />
+                <br />
+                <hr className='mx-auto w-[45%]' />
+                <br />
+                {loading ? (
+                    <LoadingSpinnerComponent type={'Spinner'} color={'black'} size={'50px'} />
+                ) : (
+                    listTask[currentPage] && listTask[currentPage].length > 0 ? (
+                        listTask[currentPage].map((item: any) => (
+                            <div key={item.task_id} className="border rounded-lg shadow p-4 text-white w-[45%] mx-auto m-5 bg-gray-900 flex justify-between items-center">
+                                <p className={`text-sm ${item.is_completed ? 'line-through' : ''}`}>{item.title}</p>
+                                <div className="flex flex-row items-center">
+                                    <TrashTask item={item} setListTask={setListTask} setTotalPage={setTotalPage} />
+                                    <MdEdit size={18} className="m-1 cursor-pointer" onClick={() => openModal(item.task_id)} />
+                                    <Completed item={item} setListTask={setListTask} setTotalPage={setTotalPage} setItemsPerPage={setItemsPerPage} setTotalItems={setTotalItems} setLimit={setLimit} />
+                                </div>
                             </div>
-                        </div>
-                    ))
-                ) : ''
-            )}
-            {loading ? '' : <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} itemsPerPage={itemsPerPage} totalItems={totalItems} totalPages={totalPage} pageLimit={5} />}
-            <Modal
-                isOpen={isModalOpen}
-                onClose={closeModal}
-                id={selectedTaskId}
-                setListTask={setListTask}
-                limit={limit}
-                setLimit={setLimit}
-                setTotalPage={setTotalPage}
-                setItemsPerPage={setItemsPerPage}
-                setTotalItems={setTotalItems}
-            />
-            <ToastContainer />
-            <br />
-            <br />
+                        ))
+                    ) : ''
+                )}
+                {loading ? '' : <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} itemsPerPage={itemsPerPage} totalItems={totalItems} totalPages={totalPage} pageLimit={5} />}
+                <Modal
+                    isOpen={isModalOpen}
+                    onClose={closeModal}
+                    id={selectedTaskId}
+                    setListTask={setListTask}
+                    limit={limit}
+                    setLimit={setLimit}
+                    setTotalPage={setTotalPage}
+                    setItemsPerPage={setItemsPerPage}
+                    setTotalItems={setTotalItems}
+                />
+                <ToastContainer />
+                <br />
+                <br />
+            </div>
         </>
     );
 }
